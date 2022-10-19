9bd1dbff95a67f6f77319d6233231d50ca6addb8
hxd-hxd <66197083+hxd-hxd@users.noreply.github.com>
2022-10-19 11:11:24 +0800

    测试建库

---
+++ Assets/Editor/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MyWindow : EditorWindow
+{
+
+    string myString = "CompleteProject";
+
+    [MenuItem("Window / Export Entire Project")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MyWindow));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Export complete Project", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Filename:", myString);
+
+        if (GUILayout.Button("Export complete Project"))
+            AssetDatabase.ExportPackage("Assets", myString + ". Unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeLibraryAssets | ExportPackageOptions.IncludeDependencies);
+    }
+
+}
